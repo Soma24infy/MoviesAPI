c80569c78531456e87a89bc59ca6faf5e794d5d6
Soma24infy <soma24mukherjee@gmail.com>
2023-01-26 21:13:54 +0300

    Add project files.

---
+++ Controllers/GenresController.cs
@@ -0,0 +1,82 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoviesAPI.DTOs;
+using MoviesAPI.Entities;
+using MoviesAPI.Helpers;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("api/genres")]
+    [ApiController]
+    public class GenresController : ControllerBase
+    {
+        private readonly ILogger<GenresController> _logger;
+        private readonly ApplicationDbContext dbContext;
+        private readonly IMapper mapper;
+
+        public GenresController(ILogger<GenresController> logger, ApplicationDbContext dbContext, IMapper mapper)
+        {
+            this._logger = logger;
+            this.dbContext = dbContext;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<GenreDTO>>> Get([FromQuery] PaginationDTO paginationDTO)
+        {
+            var queryable = dbContext.Genres.AsQueryable();
+            await HttpContext.InsertParametersPaginationInHeader(queryable);
+            var genres = await queryable.OrderBy(x=>x.Name).Paginate(paginationDTO).ToListAsync();
+            //var genres= await dbContext.Genres.ToListAsync();
+            return mapper.Map<List<GenreDTO>>(genres);
+        }
+        [HttpGet("{Id:int}")]
+        public async Task<ActionResult<GenreDTO>> Get(int Id)
+        {
+            var genre = await dbContext.Genres.FirstOrDefaultAsync(x => x.Id == Id);
+            if(genre == null)
+            {
+                return NotFound();
+            }
+            return mapper.Map<GenreDTO>(genre);
+
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(int id, [FromBody] GenreCreationDTO genreCreationDTO)
+        {
+            var genre= await dbContext.Genres.FirstOrDefaultAsync(x=>x.Id == id);
+            if(genre==null)
+            {
+                return NotFound();
+            }
+            genre = mapper.Map(genreCreationDTO, genre);
+            await dbContext.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] GenreCreationDTO genreCreationDTO)
+        {
+            var genre= mapper.Map<Genre>(genreCreationDTO);
+            dbContext.Add(genre);
+            await dbContext.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var exists = await dbContext.Genres.AnyAsync(x => x.Id == id);
+            if(!exists)
+            {
+                return NotFound();
+            }
+            dbContext.Remove(new Genre() { Id = id });
+            await dbContext.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using MoviesAPI.APIBehavior;
+using MoviesAPI.Filters;
+using MoviesAPI.Helpers;
+using NetTopologySuite;
+using NetTopologySuite.Geometries;
+
+namespace MoviesAPI
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllers(options =>
+            {
+                options.Filters.Add(typeof(MyExceptionFilter));
+                options.Filters.Add(typeof(ParseBadRequest));
+            }).ConfigureApiBehaviorOptions(BadRequestBehavior.Parse);
+            //services.AddResponseCaching();
+            services.AddAuthentication();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new() { Title = "MoviesAPI", Version = "v1" });
+            });
+            services.AddCors(options =>
+            {
+                var frontendURL = Configuration.GetValue<string>("frontend_url");
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.WithOrigins(frontendURL).AllowAnyMethod().AllowAnyHeader()
+                    .WithExposedHeaders(new string[] { "totalAmountOfRecords" });
+                });
+            });
+            services.AddAutoMapper(typeof(Startup));
+            //services.AddScoped<IFileStorageService, AzureStorageService>();
+            services.AddScoped<IFileStorageService, InAppStorageService>();
+            services.AddHttpContextAccessor();
+            services.AddSingleton(provider => new MapperConfiguration(config =>
+            {
+                var geometryFactory = provider.GetRequiredService<GeometryFactory>();
+                config.AddProfile(new AutoMapperProfiles(geometryFactory));
+            }).CreateMapper());
+
+            services.AddSingleton<GeometryFactory>(NtsGeometryServices
+                .Instance.CreateGeometryFactory(srid: 4326));
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c=>c.SwaggerEndpoint("/swagger/v1/swagger.json", "MoviesAPI v1"));
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseCors();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
